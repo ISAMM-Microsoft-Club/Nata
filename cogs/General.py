de64a9a2a6eb0b5e6e84781fdaa85ecc2a7f0a56
HammamiKhairi <hammamikhairi@gmail.com>
2022-07-16 16:17:10 +0100

    Added ping command

--- cogs/General.py
+++ cogs/General.py
@@ -12,7 +12,9 @@ class General(commands.Cog):
     if any( word in message.content.lower() for word in ['nitro', 'curse']):
       await message.delete()
 
-
+  @commands.command()
+  async def ping(self, ctx):
+    await ctx.send(f'Pong! {round(self.bot.latency * 1000)}ms')
 
 def setup(bot):
 	bot.add_cog(General(bot))
