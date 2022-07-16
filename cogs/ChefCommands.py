d5e7bce5b28d2541d098bb15386e9d7fa35fc9b8
HammamiKhairi <hammamikhairi@gmail.com>
2022-07-16 16:16:39 +0100

    Added Time Scheduling

--- cogs/ChefCommands.py
+++ cogs/ChefCommands.py
@@ -1,8 +1,9 @@
 
+import discord
 import requests
 from discord.ext import commands
 
-from .Nata.Formation import Formation
+from .Nata.Meet import Meet
 
 
 class ChefCommands(commands.Cog, name='Developer Commands'):
@@ -15,16 +16,15 @@ class ChefCommands(commands.Cog, name='Developer Commands'):
 		return ctx.author.id in self.bot.chefs_check
 
 
-	@commands.command(name="formation", aliases=['fm'])
-	async def formation(self, ctx, time):
-		await Formation( self.bot, ctx, time, ctx.author.id).confirmation()
+	@commands.command(name="meet", aliases=['fm'])
+	async def meet(self, ctx, time, link, *args):
+		if self.bot.chef_department[str(ctx.author.id)] == "Administration" and not len(args):
+			await ctx.send("Please Specify the department (only for administration)")
+			embed=discord.Embed(title="Meet Command Example", description="!meet <time> <PV link> <Department>", color=0x00ffff)
+			await ctx.send(embed=embed)
+			return
+		await Meet( self.bot, ctx, time, link, ctx.author.id, args[0]).meet()
 
-
-	@commands.command(name="formation_pv", aliases=['fmpv'])
-	async def upload_file(self, ctx):
-		attachment_url = ctx.message.attachments[0].url
-		file_request = requests.get(attachment_url)
-		await ctx.send(file_request.content)
 
 def setup(bot):
 	bot.add_cog(ChefCommands(bot))
