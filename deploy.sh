6949e9e7687b9914f908bc1101d1df2558077da3
HammamiKhairi <hammamikhairi@gmail.com>
2022-09-08 23:51:50 +0100

    untracked deployment script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-echo ======= Exporting Environment variables ========
-echo
-echo "exported : "
-export DISCORD_BOT_SECRET="********************"
-echo "   > Nata's Token"
-echo
-
-echo ======= Deploying =======
-python3 main.py
